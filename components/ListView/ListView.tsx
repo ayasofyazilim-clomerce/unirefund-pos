f6ba089eb07eb59d64fd7ec0d21974264f420796
Eren <ahmeterenodaci@gmail.com>
2025-05-13 14:31:33 +0300

    Add ListView and ListItem components for improved UI structure

---
+++ components/ListView/ListView.tsx
@@ -0,0 +1,20 @@
+import { Pressable, Text } from 'react-native';
+import { Icon } from 'react-native-paper';
+
+export function ListItem({ title, icon }: { title: string; icon: string }) {
+  return (
+    <Pressable className="mt-2 flex-row items-center rounded-lg bg-white p-3 shadow-sm active:bg-gray-50">
+      <Icon source={icon} color={'#080d19'} size={28} />
+      <Text className="ml-3 mr-auto text-lg text-[#080d19]">{title}</Text>
+      <Icon source="chevron-right" color={'#080d19'} size={32} />
+    </Pressable>
+  );
+}
+export function ListView({ title, children }: { title: string; children: React.ReactNode }) {
+  return (
+    <Pressable className="mb-8">
+      <Text className="mb-2 font-bold">{title}</Text>
+      {children}
+    </Pressable>
+  );
+}
