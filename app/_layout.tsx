f68272d56b0d3709cb0790c812abd07e5fe9c1d9
Eren <ahmeterenodaci@gmail.com>
2025-05-14 09:07:06 +0300

    Refactor RootLayout: Remove custom header component and update screen names for clarity

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,21 @@
-import '../global.css';
-import { Appbar, PaperProvider } from 'react-native-paper';
 import { Stack } from 'expo-router';
-import { NativeStackHeaderProps } from '@react-navigation/native-stack';
-import { useTheme } from 'react-native-paper';
-
-const Header = (props: NativeStackHeaderProps) => (
-  <Appbar.Header>
-    <Appbar.Content title={props.options.title} />
-    {props.options.headerRight?.({})}
-  </Appbar.Header>
-);
+import { PaperProvider, useTheme } from 'react-native-paper';
+import '../global.css';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   const theme = useTheme();
   return (
     <PaperProvider theme={{ roundness: 2, colors: { ...theme.colors, primary: '#dc0201' } }}>
+      <StatusBar style="light" />
       <Stack
         screenOptions={{
-          header: Header,
+          header: undefined,
           headerShown: false,
           contentStyle: { backgroundColor: '#fff' },
         }}>
         <Stack.Screen name="splash" />
-        <Stack.Screen name="auth" />
+        <Stack.Screen name="(public)" />
         <Stack.Screen name="(tabs)" />
       </Stack>
     </PaperProvider>
