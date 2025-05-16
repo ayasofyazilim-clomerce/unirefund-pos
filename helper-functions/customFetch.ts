e47a2737046b21eaa4839c3c6b801037425531ee
eren <ahmeterenodaci@gmail.com>
2025-05-16 14:23:03 +0300

    Add logging and error handling in token refresh process

--- helper-functions/customFetch.ts
+++ helper-functions/customFetch.ts
@@ -1,4 +1,5 @@
 import { ApiError } from '@ayasofyazilim/core-saas/AccountService';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { fetchNewAccessTokenByRefreshToken } from '~/actions/auth/actions';
 
 export async function fetchRequest<T>(request: () => Promise<T>) {
@@ -6,11 +7,17 @@ export async function fetchRequest<T>(request: () => Promise<T>) {
     const response = await request();
     return response;
   } catch (err) {
+    console.log(err);
     if (err instanceof ApiError) {
       if (err.status === 401) {
         // Refresh the token and retry the request
         if (await fetchNewAccessTokenByRefreshToken()) {
           return await fetchRequest(request);
+        } else {
+          console.error('Failed to refresh token');
+          await AsyncStorage.removeItem('refreshToken');
+          await AsyncStorage.removeItem('accessToken');
+          return new Error('Failed to refresh token');
         }
       }
       return err;
