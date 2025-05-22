3212a2bdfe42d0e70eb649ad669116002fbe8898
eren <ahmeterenodaci@gmail.com>
2025-05-22 14:07:24 +0300

    feat: add profile completion check function and update imports for better type support

--- actions/lib.ts
+++ actions/lib.ts
@@ -1,4 +1,7 @@
-import { AccountServiceClient } from '@ayasofyazilim/core-saas/AccountService';
+import {
+  AccountServiceClient,
+  Volo_Abp_Account_ProfileDto,
+} from '@ayasofyazilim/core-saas/AccountService';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HEADERS = {
@@ -10,6 +13,12 @@ export const ENVIRONMENT = {
   dev: 'http://81.213.78.69:1441',
   live: 'https://uat.unirefund.com',
 };
+
+export function isProfileCompleted(profile: Volo_Abp_Account_ProfileDto | undefined) {
+  return (
+    !!profile?.name && !!profile?.surname && !!profile.phoneNumber && !!profile.phoneNumberConfirmed
+  );
+}
 
 export async function getAccountServiceClient(customHeaders?: Record<string, string>) {
   const accessToken = (await AsyncStorage.getItem('accessToken')) || undefined;
