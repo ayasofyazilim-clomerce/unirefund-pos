2b71a163813b057cb29a58468e6541c9f1ab25ec
eren <ahmeterenodaci@gmail.com>
2025-05-28 13:51:32 +0300

    feat: integrate react-native-app-clip and update configurations

--- app-clip/App.tsx
+++ app-clip/App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { isClip, displayOverlay } from 'react-native-app-clip';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {isClip() && <Text>This is a clip</Text>}
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
