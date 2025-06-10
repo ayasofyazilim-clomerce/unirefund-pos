f8c636c8ab36aa7e762221394fbc219110676ab4
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-06-10 11:51:21 +0300

    feat:add react-native-reuseables & components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,51 @@
 import { Stack } from 'expo-router';
-import { PaperProvider, useTheme } from 'react-native-paper';
-import '../global.css';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import '~/global.css';
+import { Theme, ThemeProvider, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { NAV_THEME } from '~/lib/constants';
+import { useColorScheme } from '~/lib/useColorScheme';
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
 
 export default function RootLayout() {
-  const theme = useTheme();
+  const hasMounted = React.useRef(false);
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
+    }
+
+    if (Platform.OS === 'web') {
+      // Adds the background color to the html element to prevent white background on overscroll.
+      document.documentElement.classList.add('bg-background');
+    }
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaView className="flex-1" edges={['top']}>
-      <StatusBar style="dark" />
-      <PaperProvider theme={{ roundness: 2, colors: { ...theme.colors, primary: '#dc0201' } }}>
+      <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+        <StatusBar style="dark" />
         <Stack
           screenOptions={{
             header: undefined,
@@ -20,7 +56,9 @@ export default function RootLayout() {
           <Stack.Screen name="(public)" />
           <Stack.Screen name="(tabs)" />
         </Stack>
-      </PaperProvider>
+      </ThemeProvider>
     </SafeAreaView>
   );
 }
+const useIsomorphicLayoutEffect =
+  Platform.OS === 'web' && typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect;
